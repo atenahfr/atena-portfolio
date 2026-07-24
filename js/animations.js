404b939ab5f503a1cdb659a49d4e1a85b8ddd055
Atena <atena.hosseinifarr@gmail.com>
2026-07-23 21:48:00 -0400

    Wire up GSAP, ScrollTrigger, and Lenis smooth scroll

---
+++ js/animations.js
@@ -0,0 +1,11 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const lenis = new Lenis();
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+lenis.on('scroll', ScrollTrigger.update);
+gsap.ticker.add((time) => { lenis.raf(time * 1000); });
+gsap.ticker.lagSmoothing(0);
